b22e8e0f0f6ac04bc5335a95c26c2a1ac699b469
Rodrigo Viana <roviol@gmail.com>
2022-12-05 20:32:35 -0300

    refact: simple refact

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToItemList(pokemon) {
   return `
@@ -20,15 +17,8 @@ function convertPokemonToItemList(pokemon) {
   `;
 }
 
-const pokemonList = document.getElementById("pokemonList");
+const pokemonsList = document.getElementById("pokemonList");
 
-fetch(url)
-  .then(response => response.json())
-  .then(jsonBody => jsonBody.results)
-  .then((pokemons) => {
-    for (let index = 0; index < pokemons.length; index++) {
-      const pokemon = pokemons[index];
-      pokemonList.innerHTML += convertPokemonToItemList(pokemon);
-    }
-  })
-  .catch(error => console.log(error))
+pokeAPI.getPokemons().then((pokemons = []) => {
+  pokemonsList.innerHTML += pokemons.map(convertPokemonToItemList).join('');    
+})
