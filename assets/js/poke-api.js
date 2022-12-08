b3b0e45bf4fd73fdccb839e9619f9d149741c002
Rodrigo Viana <roviol@gmail.com>
2022-12-08 20:21:15 -0300

    refactor: list types from pokeapi

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,16 @@
 const pokeAPI = {}
 
+pokeAPI.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then(response => response.json());
+}
+
 pokeAPI.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
   return fetch(url)
-          .then(response => response.json())
-          .then(jsonBody => jsonBody.results)
-          .catch(error => console.log(error))
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeAPI.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
 }
